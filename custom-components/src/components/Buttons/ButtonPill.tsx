242e83c9cb4569ad199ddbde39fc463a3bec40f9
Nick <nsagias@gmail.com>
2022-01-14 22:47:48 -0500

    Create inverse colours and add transition

--- custom-components/src/components/Buttons/ButtonPill.tsx
+++ custom-components/src/components/Buttons/ButtonPill.tsx
@@ -4,8 +4,9 @@ import "./ButtonPill.css";
 export default function ButtonPill() {
   return (
     <>
-      <button className="button-pill success">Success</button>
       <button className="button-pill info">Info</button>
+      <button className="button-pill info-inverse">Info</button>
+      <button className="button-pill success">Success</button>
       <button className="button-pill warning">Warning</button>
       <button className="button-pill danger">Danger</button>
       <button className="button-pill default">Default</button>
