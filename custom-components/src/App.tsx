ccc1cdbab58e876ac4cc4717bd03581398c3ea9d
Nick <nsagias@gmail.com>
2022-01-14 22:31:03 -0500

    import generic button make into button pill

--- custom-components/src/App.tsx
+++ custom-components/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import ButtonPill from './components/Buttons/ButtonPill';
 
 function App() {
   return (
     <div className="App">
-  
+      <ButtonPill />
     </div>
   );
 }
--- custom-components/src/components/Buttons/ButtonPill.tsx
+++ custom-components/src/components/Buttons/ButtonPill.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./ButtonPill.css";
 
-export default function ComponentBase() {
+export default function ButtonPill() {
   return (
-    <></>
+    <>
+      <button className="button-pill success">Success</button>
+      <button className="button-pill info">Info</button>
+      <button className="button-pill warning">Warning</button>
+      <button className="button-pill danger">Danger</button>
+      <button className="button-pill default">Default</button>
+    </>
   );
 }
