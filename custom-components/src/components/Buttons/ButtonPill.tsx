1b427e7052e1ca06467c2249f5ec25b39933a7d1
Nick <nsagias@gmail.com>
2022-01-14 23:40:37 -0500

    Clean up code and save blue styling

--- custom-components/src/components/Buttons/ButtonPill.tsx
+++ custom-components/src/components/Buttons/ButtonPill.tsx
@@ -4,8 +4,8 @@ import "./ButtonPill.css";
 export default function ButtonPill() {
   return (
     <>
-      <button className="button-pill blue">Info</button>
-      <button className="button-pill blue-inverse">Info</button>
+      <button className="button-pill blue">Blue</button>
+      <button className="button-pill blue-inverse">Blue</button>
       <button className="button-pill yellow">Yellow</button>
       <button className="button-pill yellow-inverse">Yellow</button>
       <button className="button-pill default">Default</button>
