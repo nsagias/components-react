72bb0129acb814ba13b4442d2703ec03c5ceeed9
Nick <nsagias@gmail.com>
2022-01-15 18:01:17 -0500

    get starter signup form and convert to react tages

--- custom-components/src/App.tsx
+++ custom-components/src/App.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import './App.css';
 import ButtonPill from './components/Buttons/ButtonPill';
 import LoginForm from './components/LoginForm/LoginForm';
+import SignUpForm from './components/SignUpForm/SignUpForm';
 
 function App() {
   return (
     <div className="App">
-      <LoginForm />
+     <SignUpForm />
     </div>
   );
 }
--- custom-components/src/components/SignUpForm/SignUpForm.tsx
+++ custom-components/src/components/SignUpForm/SignUpForm.tsx
@@ -3,6 +3,33 @@ import "./SignUpForm.css";
 
 export default function SignUpForm() {
   return (
-    <></>
+    <>
+      <form className="signup-form" action="/action_page.php" >
+        <div className="container">
+          <h1>Sign Up</h1>
+          <p>Please fill in this form to create an account.</p>
+          <hr />
+          
+          <label><b>Email</b></label>
+          <input type="email" placeholder="Enter Email" name="email" required />
+
+          <label><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="psw" required />
+
+          <label><b>Repeat Password</b></label>
+          <input type="password" placeholder="Repeat Password" name="psw-repeat" required />
+          
+          
+          <p>By creating an account you agree to our <a className="t-c" href="#">Privacy</a>.</p>
+
+          <div className="clearfix">
+            <button type="button" className="cancelbtn">Cancel</button>
+            <button type="submit" className="signupbtn">Sign Up</button>
+          </div>
+          
+        </div>
+      </form>
+    
+    </>
   );
 }
