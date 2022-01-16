d36f10a354329a02a97cbdbc14f44179e5f00581
Nick <nsagias@gmail.com>
2022-01-16 00:40:45 -0500

    Add onClick and use handlers and setup useEffect

--- custom-components/src/components/Buttons/ButtonPillNotications/ButtonPillNotifications.tsx
+++ custom-components/src/components/Buttons/ButtonPillNotications/ButtonPillNotifications.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ButtonPillNotifications.css";
 
 export default function ButtonPillNotifications() {
+  const [blueNote, setBlueNote] = useState(0);
+  const [yellowNote, setYellowNote] = useState(0);
+
+  const blueButtonHandler = () => {
+    console.log("Blue Button Handler")
+  };
+
+  const yellowButtonHandler = () => {
+    console.log("Yellow Button Handler")
+  };
+
+  useEffect(() => {
+    // get messsages for buttons
+    // set to check for updates
+    setBlueNote(5);
+    setYellowNote(10);
+
+  }, []);
+
   return (
     <>
       <p>
-        <a href="#" className="blue-button-notification">
+        <a href="#"onClick={blueButtonHandler} className="blue-button-notification">
           <span>Inbox</span>
-          <span className="blue-button-notification-badge">3</span>
+          <span className="blue-button-notification-badge">{blueNote}</span>
         </a>
       </p>
       <p>
-        <a href="#" className="yellow-button-notification">
+        <a href="#" onClick={yellowButtonHandler} className="yellow-button-notification">
           <span>Inbox</span>
-          <span className="yellow-button-notification-badge">3</span>
+          <span className="yellow-button-notification-badge">{yellowNote}</span>
         </a>
       </p>
     </>
