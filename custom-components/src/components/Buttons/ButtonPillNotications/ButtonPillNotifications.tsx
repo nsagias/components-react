8d5137ccf6f4ffe493104aafcdc5d4e4ffc60120
Nick <nsagias@gmail.com>
2022-01-16 00:19:28 -0500

    remove dead code

--- custom-components/src/components/Buttons/ButtonPillNotications/ButtonPillNotifications.tsx
+++ custom-components/src/components/Buttons/ButtonPillNotications/ButtonPillNotifications.tsx
@@ -4,14 +4,18 @@ import "./ButtonPillNotifications.css";
 export default function ButtonPillNotifications() {
   return (
     <>
-      <a href="#" className="blue-button-notification">
-        <span>Inbox</span>
-        <span className="blue-button-notification-badge">3</span>
-      </a>
-      <a href="#" className="yellow-button-notification">
-        <span>Inbox</span>
-        <span className="yellow-button-notification-badge">3</span>
-      </a>
+      <p>
+        <a href="#" className="blue-button-notification">
+          <span>Inbox</span>
+          <span className="blue-button-notification-badge">3</span>
+        </a>
+      </p>
+      <p>
+        <a href="#" className="yellow-button-notification">
+          <span>Inbox</span>
+          <span className="yellow-button-notification-badge">3</span>
+        </a>
+      </p>
     </>
   );
 }
