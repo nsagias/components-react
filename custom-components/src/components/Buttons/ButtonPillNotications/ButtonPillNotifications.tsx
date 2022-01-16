84dfe5e62aa9f7a2836366eb8265a603ef98f3db
Nick <nsagias@gmail.com>
2022-01-15 23:56:28 -0500

    Add button notifications base

--- custom-components/src/components/Buttons/ButtonPillNotications/ButtonPillNotifications.tsx
+++ custom-components/src/components/Buttons/ButtonPillNotications/ButtonPillNotifications.tsx
@@ -5,6 +5,10 @@ export default function ButtonPillNotifications() {
   return (
     <>
       <button className="button-pill blue">Blue</button>
+      <a href="#" className="notification">
+        <span>Inbox</span>
+        <span className="badge">3</span>
+      </a>
       <button className="button-pill blue-inverse">Blue</button>
       <button className="button-pill yellow">Yellow</button>
       <button className="button-pill yellow-inverse">Yellow</button>
