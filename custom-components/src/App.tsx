d0ae2291a6bded06ca715a102c12fc48b74a64bf
Nick <nsagias@gmail.com>
2022-01-15 20:43:53 -0500

    Add base css to project

--- custom-components/src/App.tsx
+++ custom-components/src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 import './App.css';
 import ButtonPill from './components/Buttons/ButtonPill';
+import CardCarousel from './components/CardCarousel/CardCarousel';
 import LoginForm from './components/LoginForm/LoginForm';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 
 function App() {
   return (
     <div className="App">
-     <SignUpForm />
+      <CardCarousel />
     </div>
   );
 }
