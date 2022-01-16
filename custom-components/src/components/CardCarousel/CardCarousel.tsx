a76a49b197cf2ee78f65b949478b71d41b08683d
Nick <nsagias@gmail.com>
2022-01-15 20:43:31 -0500

    Add base code to file and update class to class

--- custom-components/src/components/CardCarousel/CardCarousel.tsx
+++ custom-components/src/components/CardCarousel/CardCarousel.tsx
@@ -3,6 +3,19 @@ import "./CardCarousel.css";
 
 export default function CardCarousel() {
   return (
-    <></>
+    <>
+      <div className="container">
+        <div className="carousel">
+          <div className="item a">A</div>
+          <div className="item b">B</div>
+          <div className="item c">C</div>
+          <div className="item d">D</div>
+          <div className="item e">E</div>
+          <div className="item f">F</div>
+        </div>
+      </div>
+      <div className="next">Next</div>
+      <div className="prev">Prev</div>      
+    </>
   );
 }
