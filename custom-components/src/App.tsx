3d1af0cd18bf72a1426ad12892738d7a728a3361
Nick <nsagias@gmail.com>
2022-01-15 23:44:52 -0500

    import button pill to app

--- custom-components/src/App.tsx
+++ custom-components/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import './App.css';
-import ButtonPill from './components/Buttons/ButtonPill';
+import ButtonPill from './components/Buttons/ButtonPill/ButtonPill';
+import ButtonPillNotifications from './components/Buttons/ButtonPillNotications/ButtonPillNotifications';
 import CardCarousel from './components/CardCarousel/CardCarousel';
 import LoginForm from './components/LoginForm/LoginForm';
 import SignUpForm from './components/SignUpForm/SignUpForm';
@@ -9,8 +10,7 @@ import SignUpForm from './components/SignUpForm/SignUpForm';
 function App() {
   return (
     <div className="App">
-      <CardCarousel />
-      
+      <ButtonPillNotifications />
     </div>
   );
 }
