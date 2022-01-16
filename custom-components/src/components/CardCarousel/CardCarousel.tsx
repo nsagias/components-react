51e0cc79f3da89e41c8a8c53bc47f229ba92f199
Nick <nsagias@gmail.com>
2022-01-15 21:00:43 -0500

    start conversion of jasvascript to react

--- custom-components/src/components/CardCarousel/CardCarousel.tsx
+++ custom-components/src/components/CardCarousel/CardCarousel.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import Reac, { useState } from "react";
 import "./CardCarousel.css";
 
 export default function CardCarousel() {
+
+  const [curentDeg, setCurrentDeg] = useState("");
+
+
+  
+  const handleClickNext = () => {
+    currdeg = currdeg - 60;
+   // "transform": "rotateY("+currdeg+"deg)";
+  };
+  const handleClickPrev = () => {
+    currdeg = currdeg + 60;
+    //"transform": "rotateY("+currdeg+"deg)";
+  };
+
+
+ let carousel = (".carousel")
+ let currdeg  = 0;
+
+// $(".next").on("click", { d: "n" }, rotate);
+// $(".prev").on("click", { d: "p" }, rotate);
+
+//  function rotate () {
+
+//   if(event.data.d=="n"){
+
+//     currdeg = currdeg - 60;
+//   }
+//   if(event.data.d=="p"){
+//     currdeg = currdeg + 60;
+//   }
+//   carousel.css({
+//     "transform": "rotateY("+currdeg+"deg)"
+//   });
+// }
+
   return (
     <>
       <div className="container">
