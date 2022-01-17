517fefdccc650d0c7c3060040b2cabc4e1f26b17
Nick <nsagias@gmail.com>
2022-01-16 23:40:36 -0500

    Remove dead code and format code

--- custom-components/src/components/Tables/Table/Table.tsx
+++ custom-components/src/components/Tables/Table/Table.tsx
@@ -3,8 +3,6 @@ import "./Table.css";
 
 export default function Table() {
 
-
-
   const data = {
     tableName : "A Table",
     tableHeaderList: [ 
@@ -26,20 +24,7 @@ export default function Table() {
       { id: 9, company: "I.O.", contact: "Any Myth", country: "Greece" }
     ]
   };
-  const contactList2 =
-  [
-    { id: 1, company: "Alfreds", contact: "Mary Mary", country: "Canada" },
-    { id: 2, company: "Bergman", contact: "Chstina Tina", country: "France" },
-    { id: 3, company: "Charlie", contact: "Bob Dingo", country: "Australia" },
-    { id: 4, company: "Darwin", contact: "Dr. Darwin", country: "United Kingdom" },
-    { id: 5, company: "Ever Fun", contact: "Fun Police", country: "United States" },
-    { id: 6, company: "Form and Able", contact: "Stundent Somewhere", country: "Spain" },
-    { id: 7, company: "Gregarious Social", contact: "QRCode Avatar", country: "Norway" },
-    { id: 8, company: "Hip", contact: "Everyone is cool here", country: "Italy" },
-    { id: 9, company: "I.O.", contact: "Any Myth", country: "Greece" }
-  ]
-
-  
+ 
   const tableName = data.tableName;
   const tableCaption = data.tableCaption;
   const headerList = data.tableHeaderList;
@@ -55,17 +40,13 @@ export default function Table() {
       <td>{contact.contact}</td>
       <td>{contact.country}</td>
     </tr>
-));
-
+  ));
 
 
   return (
     <div>
     <h1>{tableName}</h1>
-    
-   
       <table className="customers">
-      
       <caption>{tableCaption}</caption>
         <tbody>
         <tr>
@@ -73,7 +54,6 @@ export default function Table() {
         </tr>
          {parsedContactList}   
         </tbody>
-
       </table>
     </div>
   );
