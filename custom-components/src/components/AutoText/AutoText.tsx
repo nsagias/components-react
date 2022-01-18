2414c7118305940f401043d550242e0abeacf81f
Nick <nsagias@gmail.com>
2022-01-17 23:21:57 -0500

    add looping code

--- custom-components/src/components/AutoText/AutoText.tsx
+++ custom-components/src/components/AutoText/AutoText.tsx
@@ -5,7 +5,21 @@ export default function AutoText() {
 
   const autoText = "Auto Text Effect...";
   let autoTextIndex = 1;
-  let autoTextShow = autoText.slice(0, autoTextIndex);
+  // let autoTextShow = autoText.slice(0, autoTextIndex);
+  let autoTextShow;
+
+  const autoTextWriter = () => {
+    autoTextShow = autoText.slice(0, autoTextIndex);
+    autoTextIndex++;
+
+    if (autoTextIndex > autoText.length) {
+;
+    }
+    setTimeout(autoTextWriter, 1000)
+
+  }
+ 
+  autoTextWriter();
 
 
   return (
