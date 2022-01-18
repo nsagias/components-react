275d330d888e1222050732736e879d060f898f00
Nick <nsagias@gmail.com>
2022-01-17 22:26:34 -0500

    Add h1, label, and create input

--- custom-components/src/components/AutoText/AutoText.tsx
+++ custom-components/src/components/AutoText/AutoText.tsx
@@ -3,6 +3,20 @@ import "./AutoText.css";
 
 export default function AutoText() {
   return (
-    <></>
+    <>
+      <h1 id="text">Auto Text Effect...</h1>
+      <div>
+        <label htmlFor="speed">Speed</label>
+        <input 
+          type="number"
+          name="speed"
+          id="speed"
+          value="1"
+          min="1"
+          max="5"
+          step="1"
+        />
+      </div>
+    </>
   );
 }
