919d51bbf061b3ca4609dc7450faa4608067a0f2
Nick <nsagias@gmail.com>
2022-01-18 23:50:35 -0500

    Scaffold empty component MobileMenu and import css

--- custom-components/src/App.tsx
+++ custom-components/src/App.tsx
@@ -5,13 +5,14 @@ import ButtonPill from './components/Buttons/ButtonPill/ButtonPill';
 import ButtonPillNotifications from './components/Buttons/ButtonPillNotications/ButtonPillNotifications';
 import CardCarousel from './components/CardCarousel/CardCarousel';
 import LoginForm from './components/LoginForm/LoginForm';
+import MobileMenu from './components/Menus/MobileMenu/MobileMenu';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import Table from './components/Tables/Table/Table';
 
 function App() {
   return (
     <div className="App">
-      <Table />
+      <MobileMenu />
     </div>
   );
 }
---
+++ custom-components/src/components/Menus/MobileMenu/MobileMenu.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./MobileMenu.css"
+
+export default function MobileMenu() {
+  return (
+    <></>
+  );
+}
