1b7a7adb8330f0ebead1154f88e803f3909c9754
Nick <nsagias@gmail.com>
2022-01-19 01:01:21 -0500

    Add padding to main area

--- custom-components/src/components/Menus/MobileMenu/MobileMenu.tsx
+++ custom-components/src/components/Menus/MobileMenu/MobileMenu.tsx
@@ -15,7 +15,7 @@ export default function MobileMenu() {
             <i className="fa fa-bars"></i>
           </a>
 
-        <div>
+        <div className="container-content">
           <h3>Vertical Mobile Navbar</h3>
           <p>This example demonstrates how a navigation menu on a mobile/smart phone could look like.</p>
           <p>Click on the hamburger menu (three bars) in the top right corner, to toggle the menu.</p>
