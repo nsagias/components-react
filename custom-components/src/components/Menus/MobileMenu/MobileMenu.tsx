770ca10dbde5d6654b1fda6b23d2c98daa4f52a6
Nick <nsagias@gmail.com>
2022-01-19 01:04:56 -0500

    Add topnav div

--- custom-components/src/components/Menus/MobileMenu/MobileMenu.tsx
+++ custom-components/src/components/Menus/MobileMenu/MobileMenu.tsx
@@ -5,16 +5,17 @@ export default function MobileMenu() {
   return (
     <>
     <div className="mobile-container">
+    <div className="topnav">
     <a href="#home" className="active">Logo</a>
       <div id="myLinks">
         <a href="#news">News</a>
         <a href="#contact">Contact</a>
         <a href="#about">About</a>
       </div>
-          <a href="javascript:void(0);" className="icon" >
-            <i className="fa fa-bars"></i>
-          </a>
-
+      <a href="javascript:void(0);" className="icon" >
+        <i className="fa fa-bars"></i>
+      </a>
+    </div>
         <div className="container-content">
           <h3>Vertical Mobile Navbar</h3>
           <p>This example demonstrates how a navigation menu on a mobile/smart phone could look like.</p>
