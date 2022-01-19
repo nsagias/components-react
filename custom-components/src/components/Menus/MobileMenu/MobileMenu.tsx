3ab8c9764bec3bc9e88ffaf661d13c7581cbeaca
Nick <nsagias@gmail.com>
2022-01-19 00:03:07 -0500

    Add basic html

--- custom-components/src/components/Menus/MobileMenu/MobileMenu.tsx
+++ custom-components/src/components/Menus/MobileMenu/MobileMenu.tsx
@@ -3,6 +3,26 @@ import "./MobileMenu.css"
 
 export default function MobileMenu() {
   return (
-    <></>
+    <>
+    <div className="mobile-container">
+    <a href="#home" className="active">Logo</a>
+      <div id="myLinks">
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
+          <a href="javascript:void(0);" className="icon" >
+            <i className="fa fa-bars"></i>
+          </a>
+
+        <div>
+          <h3>Vertical Mobile Navbar</h3>
+          <p>This example demonstrates how a navigation menu on a mobile/smart phone could look like.</p>
+          <p>Click on the hamburger menu (three bars) in the top right corner, to toggle the menu.</p>
+        </div>
+
+     </div>
+
+    </>
   );
 }
